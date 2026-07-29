23ccfc45a506223f727e01cb5332ea703efbd953
VenkiInnocito <venkatesh.mirithipati@innocito.com>
2026-07-29 11:00:22 +0530

    C:/Program Files/Git/me API fix

--- src/app/chat/ChatFooterComponent.tsx
+++ src/app/chat/ChatFooterComponent.tsx
@@ -88,10 +88,15 @@ export default function ChatFooterComponent() {
         },
       );
       if (response?.allowed) {
-        localStorage.setItem("AI_ID", apiKey);
+        window.localStorage.setItem("AI_ID", apiKey);
+        window.localStorage.setItem("allowed", response?.allowed);
         // Fetch authenticated user information
         const userInfo = await chatService.getUserInfo();
-        window.localStorage.setItem("USER_ID", userInfo?.owner_user_id);
+        if (userInfo?.owner_user_id) {
+          window.localStorage.setItem("USER_ID", userInfo?.owner_user_id);
+        } else {
+          console.error("owner_user_id not found", userInfo);
+        }
         window.dispatchEvent(
           new CustomEvent("gatewayValidationStatus", {
             detail: { allowed: true },
