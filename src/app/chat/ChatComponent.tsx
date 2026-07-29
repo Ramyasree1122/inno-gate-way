e6c56c901df9eca24e12edd7a4383256933221d0
VenkiInnocito <venkatesh.mirithipati@innocito.com>
2026-07-29 12:10:42 +0530

    removed the  commented code and define the props for the chatSessions

--- src/app/chat/ChatComponent.tsx
+++ src/app/chat/ChatComponent.tsx
@@ -7,7 +7,7 @@ type Chat = {
   title: string;
 };
 interface props {
-  chatSessions: [];
+  chatSessions: ChatSession[];
 }
 export interface ChatSession {
   id: string;
@@ -17,26 +17,16 @@ export interface ChatSession {
   updated_at: string;
   last_message_at: string;
 }
-export default function ChatComponent({ chatSessions }) {
+export default function ChatComponent({ chatSessions }: props) {
   const router = useRouter();
 
   const pathname = usePathname();
 
-  const [chats, setChats] = useState<Chat[]>([
-    {
-      id: crypto.randomUUID(),
-      title: "Welcome to InnoAIGateway",
-    },
-  ]);
-  const handleNewChat = () => {
-    // const newChat = {
-    //   id: crypto.randomUUID(),
-    //   title: `New Chat ${chats.length}`,
-    // };
+  const [chats, setChats] = useState<Chat[]>([]);
 
-    // setChats((prev) => [newChat, ...prev]);
+  const handleNewChat = () => {
     router.push(`/chat`);
-  };;
+  };
 
   return (
     <nav className="flex-1">
@@ -54,7 +44,6 @@ export default function ChatComponent({ chatSessions }) {
       <div className="max-h-87 overflow-y-auto">
         {chatSessions?.map((chat: ChatSession) => {
           const href = "";
-          // `/chat/${chat.id}`;
           const isActive = pathname === href;
 
           return (
